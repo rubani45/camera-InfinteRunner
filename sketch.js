2649f561e03f2a56721e49711d0df7083f7727ef
rubani45 <77843138+rubani45@users.noreply.github.com>
2021-07-13 20:48:24 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,36 +3,38 @@ var END = 0;
 var gameState = PLAY;
 
 var trex, trex_running, trex_collided;
-var ground, invisibleGround, groundImage;
+var ground, invisibleGround, groundImage, bgImg;
 
 var cloudsGroup, cloudImage;
 var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
 var score;
 var gameOverImg,restartImg
-var jumpSound , checkPointSound, dieSound
+var jumpSound , checkPointSound, dieSound;
 
 function preload(){
-  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
-  trex_collided = loadAnimation("trex_collided.png");
+  trex_running = loadAnimation("images/trex1.png","images/trex3.png","images/trex4.png");
+  trex_collided = loadAnimation("images/trex_collided.png");
   
-  groundImage = loadImage("ground2.png");
+  groundImage = loadImage("images/ground2.png");
   
-  cloudImage = loadImage("cloud.png");
+  cloudImage = loadImage("images/cloud.png");
   
-  obstacle1 = loadImage("obstacle1.png");
-  obstacle2 = loadImage("obstacle2.png");
-  obstacle3 = loadImage("obstacle3.png");
-  obstacle4 = loadImage("obstacle4.png");
-  obstacle5 = loadImage("obstacle5.png");
-  obstacle6 = loadImage("obstacle6.png");
+  obstacle1 = loadImage("images/obstacle1.png");
+  obstacle2 = loadImage("images/obstacle2.png");
+  obstacle3 = loadImage("images/obstacle3.png");
+  obstacle4 = loadImage("images/obstacle4.png");
+  obstacle5 = loadImage("images/obstacle5.png");
+  obstacle6 = loadImage("images/obstacle6.png");
   
-  restartImg = loadImage("restart.png")
-  gameOverImg = loadImage("gameOver.png")
+  restartImg = loadImage("images/restart.png");
+  gameOverImg = loadImage("images/gameOver.png");
   
-  jumpSound = loadSound("jump.mp3")
-  dieSound = loadSound("die.mp3")
-  checkPointSound = loadSound("checkPoint.mp3")
+  bgImg = loadImage("images/bg.png");
+
+  jumpSound = loadSound("sounds/jump.mp3")
+  dieSound = loadSound("sounds/die.mp3")
+  checkPointSound = loadSound("sounds/checkPoint.mp3")
 }
 
 function setup() {
@@ -79,8 +81,9 @@ function setup() {
 
 function draw() {
   
-  background(180);
+  background(bgImg);
   //displaying score
+  fill("yellow");
   text("Score: "+ score, 500,50);
   
   camera.position.x = trex.x + 300;
@@ -150,9 +153,19 @@ function draw() {
      if(mousePressedOver(restart)) {
       reset();
     }
-   }
+   } else if(gameState === totalEnd){
+      obstaclesGroup.setVelocityXEach(0);
+      cloudsGroup.setVelocityXEach(0);
+      ground.velocityX = 0;
+      trex.changeAnimation("collided", trex_collided);
+      trex.velocityY = 0;
+      obstaclesGroup.setLifetimeEach(-1);
+      cloudsGroup.setLifetimeEach(-1);
+      invisibleGround.velocityX = 0;
+      text("Game End", camera.position.x - 200, 100);
+   } 
+
   
- 
   //stop trex from falling down
   trex.collide(invisibleGround);
   
@@ -183,7 +196,7 @@ function reset(){
 }
 
 function spawnObstacles(){
- if (frameCount % 60 === 0){
+ if (camera.position.x % 150 === 0){
    var obstacle = createSprite(camera.position.x + 950,165,10,40);
    obstacle.velocityX = -(6 + score/100);
    
@@ -216,7 +229,7 @@ function spawnObstacles(){
 
 function spawnClouds() {
   //write code here to spawn the clouds
-  if (frameCount % 60 === 0) {
+  if (camera.position.x % 150 === 0) {
     var cloud = createSprite(camera.position.x + 950,120,40,10);
     cloud.y = Math.round(random(80,120));
     cloud.addImage(cloudImage);
